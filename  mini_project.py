bfe81064c6bdeaea3a18663a7f88f1e90a85cb4c
IsthatEmma Bautista <emmabautista.2008@gmail.com>
2024-09-11 17:46:55 +0000

    calculating space calculator

---
+++  mini_project.py
@@ -0,0 +1,16 @@
+print("welcome to the space travel calculator")
+distance = input ("What is your distance of your destination?")
+distance = float(134)
+print(distance *10)
+
+print("your distance of destination is converted to " + str(distance * 10))
+
+speed = input("What is the speed of your spaceship?")
+speed = float(8/5)
+print(speed * 5) 
+print("your spaceship speed is converted to " + str(speed * 5))
+
+time = input("time of calculation")
+time = float(134/8/5)
+print(time * 5)
+print("it would take you " + str(time * 5) + "hours to reach to your destination")
